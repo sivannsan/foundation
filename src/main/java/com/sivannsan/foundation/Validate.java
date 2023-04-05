86331dbb6aaf0e68aa86196ac58227295ced346f
Sivann San <sivannsan73@gmail.com>
2023-04-05 22:40:11 +0700

    Update Validate class with adding some nonnegative methods

--- src/main/java/com/sivannsan/foundation/Validate.java
+++ src/main/java/com/sivannsan/foundation/Validate.java
@@ -6,10 +6,52 @@ import java.util.Objects;
 
 public final class Validate {
     public static <T> T nonnull(T t) {
-        return Objects.requireNonNull(t);
+        if (t != null) return t;
+        throw new RuntimeException("An object is null!");
     }
 
     public static <T> T nonnull(T t, @Nonnull String message) {
-        return Objects.requireNonNull(t, message);
+        if (t != null) return t;
+        throw new RuntimeException(message);
+    }
+
+    public static int nonnegative(int n) {
+        if (n >= 0) return n;
+        throw new RuntimeException("An int32 is negative!");
+    }
+
+    public static int nonnegative(int n, @Nonnull String message) {
+        if (n >= 0) return n;
+        throw new RuntimeException(message);
+    }
+
+    public static long nonnegative(long n) {
+        if (n >= 0) return n;
+        throw new RuntimeException("An int64 is negative!");
+    }
+
+    public static long nonnegative(long n, @Nonnull String message) {
+        if (n >= 0) return n;
+        throw new RuntimeException(message);
+    }
+
+    public static float nonnegative(float n) {
+        if (n >= 0) return n;
+        throw new RuntimeException("An float32 is negative!");
+    }
+
+    public static float nonnegative(float n, @Nonnull String message) {
+        if (n >= 0) return n;
+        throw new RuntimeException(message);
+    }
+
+    public static double nonnegative(double n) {
+        if (n >= 0) return n;
+        throw new RuntimeException("An float64 is negative!");
+    }
+
+    public static double nonnegative(double n, @Nonnull String message) {
+        if (n >= 0) return n;
+        throw new RuntimeException(message);
     }
 }
