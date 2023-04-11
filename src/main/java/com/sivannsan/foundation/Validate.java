61340d1378d964ad45e4f7538cf8897a0c9cc8a2
Sivann San <sivannsan73@gmail.com>
2023-04-11 17:21:02 +0700

    Add missing annotations in Validate class

--- src/main/java/com/sivannsan/foundation/Validate.java
+++ src/main/java/com/sivannsan/foundation/Validate.java
@@ -2,17 +2,20 @@ package com.sivannsan.foundation;
 
 import com.sivannsan.foundation.annotation.Nonnull;
 
-import java.util.Objects;
-
 public final class Validate {
+    private Validate() {
+    }
+
+    @Nonnull
     public static <T> T nonnull(T t) {
         if (t != null) return t;
         throw new RuntimeException("An object is null!");
     }
 
+    @Nonnull
     public static <T> T nonnull(T t, @Nonnull String message) {
         if (t != null) return t;
-        throw new RuntimeException(message);
+        throw new RuntimeException(nonnull(message));
     }
 
     public static int nonnegative(int n) {
@@ -22,7 +25,7 @@ public final class Validate {
 
     public static int nonnegative(int n, @Nonnull String message) {
         if (n >= 0) return n;
-        throw new RuntimeException(message);
+        throw new RuntimeException(nonnull(message));
     }
 
     public static long nonnegative(long n) {
@@ -32,26 +35,26 @@ public final class Validate {
 
     public static long nonnegative(long n, @Nonnull String message) {
         if (n >= 0) return n;
-        throw new RuntimeException(message);
+        throw new RuntimeException(nonnull(message));
     }
 
     public static float nonnegative(float n) {
         if (n >= 0) return n;
-        throw new RuntimeException("An float32 is negative!");
+        throw new RuntimeException("A float32 is negative!");
     }
 
     public static float nonnegative(float n, @Nonnull String message) {
         if (n >= 0) return n;
-        throw new RuntimeException(message);
+        throw new RuntimeException(nonnull(message));
     }
 
     public static double nonnegative(double n) {
         if (n >= 0) return n;
-        throw new RuntimeException("An float64 is negative!");
+        throw new RuntimeException("A float64 is negative!");
     }
 
     public static double nonnegative(double n, @Nonnull String message) {
         if (n >= 0) return n;
-        throw new RuntimeException(message);
+        throw new RuntimeException(nonnull(message));
     }
 }
