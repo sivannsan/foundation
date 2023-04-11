c4e1f953d1ea59d7ee8cd14388bd073ce4467f7d
Sivann San <sivannsan73@gmail.com>
2023-04-11 17:21:44 +0700

    Add ifNegative methods to Ensure class

--- src/main/java/com/sivannsan/foundation/Ensure.java
+++ src/main/java/com/sivannsan/foundation/Ensure.java
@@ -1,9 +1,30 @@
 package com.sivannsan.foundation;
 
+import com.sivannsan.foundation.annotation.Nonnegative;
 import com.sivannsan.foundation.annotation.Nonnull;
 
-public class Ensure {
+public final class Ensure {
+    private Ensure() {
+    }
+
+    @Nonnull
     public static <T> T ifNull(T t, @Nonnull T nonnullT) {
         return t == null ? Validate.nonnull(nonnullT) : t;
+    }
+
+    public static int ifNegative(int n, @Nonnegative int nonnegativeN) {
+        return n < 0 ? Validate.nonnegative(n) : n;
+    }
+
+    public static long ifNegative(long n, @Nonnegative long nonnegativeN) {
+        return n < 0 ? Validate.nonnegative(n) : n;
+    }
+
+    public static float ifNegative(float n, @Nonnegative float nonnegativeN) {
+        return n < 0 ? Validate.nonnegative(n) : n;
+    }
+
+    public static double ifNegative(double n, @Nonnegative double nonnegativeN) {
+        return n < 0 ? Validate.nonnegative(n) : n;
     }
 }
