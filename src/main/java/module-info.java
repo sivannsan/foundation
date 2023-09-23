03abd70c27886fb2978cda82a03bbf2aacd08da2
Sivann San <sivannsan73@gmail.com>
2023-09-23 23:25:09 +0700

    Enter the modular system

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module sivannsan.foundation {
+    exports com.sivannsan.foundation;
+    exports com.sivannsan.foundation.annotation;
+    exports com.sivannsan.foundation.common;
+    exports com.sivannsan.foundation.utility;
+}
